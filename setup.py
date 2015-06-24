967563040c7d03a640c89dbc63e784d767a0b5dd
Ashwini Patil <apatil1@jhu.edu>
2015-06-24 15:03:26 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# Get version number from package
+exec(open('pathfinderlib/version.py').read())
+
+setup(
+    name='PathFinder',
+    version=__version__,
+    description='A short wrapper for finding presence/absence/abundance of pathways from metagenomic data.',
+    author='Ashwini Patil',
+    author_email='patil.ashwini1091@gmail.com',
+    url='https://github.com/PennChopMicrobiomeProgram',
+    packages=['pathfinderlib'],
+    scripts=['scripts/pathfinder.py'],
+    )
